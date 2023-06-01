f05809b6e1082cba85cefcde0bef178b7fd5de68
zzangsally <zzangsally@naver.com>
2023-06-01 11:58:54 +0900

    KAKAO LOGIN

--- src/main/java/com/sos/signal/SignalApplication.java
+++ src/main/java/com/sos/signal/SignalApplication.java
@@ -2,8 +2,9 @@ package com.sos.signal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SignalApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +12,6 @@ public class SignalApplication {
 	}
 
 }
+
+
+

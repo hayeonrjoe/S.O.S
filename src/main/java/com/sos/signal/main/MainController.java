0b21507e172151f6112f3932de6a906c52845a40
Becca <123378408+beccajoe@users.noreply.github.com>
2023-06-03 13:45:46 +0900

    Added a main controller for the main page

---
+++ src/main/java/com/sos/signal/main/MainController.java
@@ -0,0 +1,30 @@
+package com.sos.signal.main;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/home")
+public class MainController {
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @GetMapping
+    public ResponseEntity<Resource> showMain() throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:templates/common/main.html");
+        if (resource.exists()) {
+            return ResponseEntity.ok().contentType(MediaType.TEXT_HTML).body(resource);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}

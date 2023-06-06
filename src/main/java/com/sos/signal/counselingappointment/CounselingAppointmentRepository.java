9035537573290eb226635155364a7a61ce47e044
Becca <123378408+beccajoe@users.noreply.github.com>
2023-06-06 13:00:54 +0900

    Updated link for the header and main

--- src/main/java/com/sos/signal/counselingappointment/CounselingAppointmentRepository.java
+++ src/main/java/com/sos/signal/counselingappointment/CounselingAppointmentRepository.java
@@ -4,6 +4,6 @@ package com.sos.signal.counselingappointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // Repository interface for database operations
-public interface CounselingAppointmentRepository extends JpaRepository<CounselingAppointment, Long> {
+public interface CounselingAppointmentRepository extends JpaRepository<CounselingAppointment, Integer> {
     // Add any custom methods if needed
 }
--- src/main/java/com/sos/signal/policecomplaint/repository/PoliceComplaintRepository.java
+++ src/main/java/com/sos/signal/policecomplaint/repository/PoliceComplaintRepository.java
@@ -4,6 +4,6 @@ import com.sos.signal.policecomplaint.dto.PoliceComplaint;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // Repository interface for database operations
-public interface PoliceComplaintRepository extends JpaRepository<PoliceComplaint, Long> {
+public interface PoliceComplaintRepository extends JpaRepository<PoliceComplaint, Integer> {
     // Add any custom methods if needed
 }

09d6f92094f1d24b1f715b5d82e566b83d4f4ab8
zzangsally <zzangsally@naver.com>
2023-06-07 17:25:55 +0900

    view mapping

--- src/main/java/com/sos/signal/admin/AdminController.java
+++ src/main/java/com/sos/signal/admin/AdminController.java
@@ -15,7 +15,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/register-form")

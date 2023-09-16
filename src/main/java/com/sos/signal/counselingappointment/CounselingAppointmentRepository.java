eb6d49e8d0f6f93e928c6c33b967b42315dc7a64
Becca <123378408+beccajoe@users.noreply.github.com>
2023-09-16 13:33:46 +0900

    Update css for success page after submitting counseling appointment form

--- src/main/java/com/sos/signal/counselingappointment/CounselingAppointmentRepository.java
+++ src/main/java/com/sos/signal/counselingappointment/CounselingAppointmentRepository.java
@@ -3,7 +3,6 @@ package com.sos.signal.counselingappointment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// Repository interface for database operations
 public interface CounselingAppointmentRepository extends JpaRepository<CounselingAppointment, Integer> {
-    // Add any custom methods if needed
+
 }
